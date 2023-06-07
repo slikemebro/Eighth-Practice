fb55ea9e205bc0c48b7a6971978926ab3a2d6d21
Gleb Korobov <gkorobov9@gmail.com>
2023-06-07 11:59:43 +0300

    first commit empty project

---
+++ src/main/java/com/ua/glebkorobov/eighthpractice/EighthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ua.glebkorobov.eighthpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EighthPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EighthPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/glebkorobov/eighthpractice/EighthPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.glebkorobov.eighthpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EighthPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
